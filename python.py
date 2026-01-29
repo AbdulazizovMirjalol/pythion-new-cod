1a7d3638fd614b522ba3302ca84b665248bf8e97
Mirjalol <mirjalolabdulazizov837@gmail.com>
2026-01-29 23:50:23 +0500

    new code

---
+++ python.py
@@ -0,0 +1,192 @@
+# age = 19 
+# password = True
+
+# if age >= 29 and password:
+#     print("Krisish mumkin")
+# else:
+#     print("sizga mumkkin emas")
+
+# stundent = True
+# pensioner = True
+
+# if stundent or pensioner:
+#     print("sizga chegirma bor")
+# else:
+#     print("afsuski sizga chegirma yo'q")
+
+# ligin = False
+
+# if not ligin:
+#     print("avval login qiling")
+
+# s = "Hi my Name is Mirjalol"
+# print(s.upper())
+
+
+# words=["Hi", "Mother", "Father"]
+# print("Brother".join(words))
+# s = "salom"
+# print(len(s))
+
+# age = 18
+# if age >= 18:
+#     print ("katta")
+# else:
+#     print("kichik")
+
+# ball = 70
+
+# if ball >= 90:
+#     print("A")
+# elif ball >= 70:
+#     print("B")
+# elif ball >= 60:
+#     print("C")
+# else:
+#     print("you feiled")
+
+# age = 10
+# password = True
+
+# if age >= 18:
+#     if password:
+#         print("kirishingiz mumkin")
+#     else:
+#         print("password yo'q")
+# else:
+#     print("yosh yetmaydi")
+
+# age = 19
+# password = False
+
+# if age >= 18 and password:
+#     print("krishingiz mumkin")
+# elif age >= 18 and not password:
+#     print("password yo'q")
+# else:
+#     print("yosh yetarli emas")
+
+# x = 8
+
+# if x >= 0:
+#     print("musbat")
+# elif x == 0:
+#     print("Nol")
+# else:
+#     print("manfiy")
+
+# nums = [1,2,3,4,5]
+# i = nums
+# for i in range(5):
+#     print(i)
+
+# for i in range(10):
+#     print(i)
+
+# i = 10
+# while i <= 5:
+#     print(i)
+#     i += 1 
+
+# parol = "1234"
+# k = input("Parol kriting: ")
+
+# while k != parol:
+#     k =input("Qayta ter ")
+# print("Kirdi")
+# for i in range(10):
+#     if i == 8:
+#         break
+#     print(i) 
+
+# for i in range(1,15):
+#     if i == 3:
+#         continue
+#     print(i)
+
+# for i in range(100):
+#     if i % 2 == 0:
+#         print("Juft son", i)
+    
+# s = 0
+
+# for i in range(1,101):
+#     s += i 
+# print(s)
+
+# num = [1,3,4,6,-9,1,3,-3,4,2]
+
+# for i in num:
+#     if i < 0:
+#         print("Musbat sonlar topildi" , i)
+#         break
+
+# class animal:
+#     def sound(self):
+#         print("ovoz chiqaradi")
+# class dog(animal):
+#     pass
+
+# Dog = dog ()
+# Dog.sound()
+
+
+# class car:
+#     def drive(self):
+#         print("yuradi")
+# class malibu(car):
+#     pass
+
+
+# cars = malibu()
+# cars.d
+
+# class Student:
+#     def __init__(self, name , surname, ball ):
+#         self.name=name
+#         self.suname=surname
+#         self.ball=ball
+#     def baho(self):
+#         if self.ball>0:
+#             return "Good"
+# student = Student("Mirjalol", "Abdulzizov", 28)
+# print(student.baho())
+
+
+# class student:
+#     def __init__(self, name,surname,baho):
+#         self.name=name
+#         self.surname=surname
+#         self.baho=baho
+#     def check(self):
+#         if self.baho <= 80:
+#             return "Mirjalol"
+#         else:
+#             return "Ibrohim"
+# talaba = student("Mirjalol","Abdulazizov",90)
+# print(talaba.check())
+    
+
+# class student:
+#     def __init__(self,name,surname,gread):
+#         self.name = name
+#         self.surname = surname
+#         self.gread = gread
+#     def bahosi(self):
+#         if self.gread >= 80:
+#             return "good"
+#         else:
+#             return "bad"
+# talaba = student("Mirjalol", "Abdulazizov", 90)
+# print(talaba.bahosi())
+        
+# class student:
+#     def __init__(self,name,baho):
+#         self.name = name
+#         self.baho = baho
+#     def bahosi(self):
+#         if self.baho >= 90:
+#             return "pass"
+# talaba = student ('mirjalol',100)
+# print(talaba.bahosi())
+
